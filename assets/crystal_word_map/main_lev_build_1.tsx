694d17005e0faf24dcd71edac99b1354312ee645
VietTQ20 <viettq20@fpt.com.vn>
2021-04-19 16:00:43 +0700

    commit

---
+++ assets/crystal_word_map/main_lev_build_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="main_lev_build_1" tilewidth="16" tileheight="16" tilecount="4096" columns="64">
+ <image source="../main_lev_build_1.png" width="1024" height="1024"/>
+</tileset>
---
+++ src/scenes/Preload.js
@@ -0,0 +1,19 @@
+import Phaser from 'phaser';
+
+class Preload extends Phaser.Scene {
+    constructor() {
+        super('PreloadScene');
+    }
+
+    preload() {
+        this.load.tilemapTiledJSON('map', 'assets/crystal_world_map.json');
+        this.load.image('tiles-1', '../../assets/main_lev_build_1.png');
+        this.load.image('tiles-2', '../../assets/main_lev_build_2.png');
+    }
+
+    create() {
+        this.add.image(0, 0, 'sky').setOrigin(0);
+    }
+}
+
+export default PlayScene;
